033fb7632bf2128e5fca5c1a2daa99a5a2c2897f
gr0kchain <gr0kchain@bitcoindev.network>
2020-03-30 13:40:54 +0200

    Initial commit

---
+++ scraper.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const artoo = require("artoo-js");
+const cheerio = require("cheerio");
+const notifier = require("node-notifier")
+const fs = require('fs');
+
+artoo.bootstrap(cheerio);
+
+axios.get("https://sacoronavirus.co.za/").then(function(response) {
+
+	// read prev data, cached in repo for comparison
+	const prevData = fs.readFileSync(process.cwd() + '/data.json','utf-8')
+
+	var $ = cheerio.load(response.data)
+
+	// parse and extract data points
+	var data = $('span[data-value]').scrape({ value: 'data-value' })
+
+
+	// Setup payload object
+	var payload = {}
+
+	payload.concluded = data[0]
+	payload.positive = data[1]
+	payload.recoveries = data[2]
+	payload.deahts = data[3]
+
+	var payloadString = JSON.stringify(payload)
+
+	if (prevData == payloadString) {
+		// Do nothing
+  } else {
+		// Do something
+	}
+
+	fs.writeFileSync(process.cwd() + '/data.json', JSON.stringify(payload));
+})
